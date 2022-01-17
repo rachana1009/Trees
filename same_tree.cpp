48a9b196cc2df48e9ed456ec1f13ed3de41ae7cb
Rachana <rachana100902@gmail.com>
2022-01-17 18:09:13 +0530

    leetcode solution of same tree

---
+++ same_tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        while(p->right!=NULL && q->right!=NULL)
+        {
+            if(p->val==q->val)
+            {
+                p=p->left;
+                q=q->left;
+            }
+            else 
+                return false;
+        }
+        return true;
+    }
+};
