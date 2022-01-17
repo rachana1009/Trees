52df7a538bb3b1832cbcb4fcb794c7274df37ed2
Rachana <rachana100902@gmail.com>
2022-01-17 18:08:34 +0530

    bst operations

---
+++ bst.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+    public:
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeeNode()
+    {
+        value=0;
+        left=NULL;
+        right=NULL;
+    }
+
+    TreeNode(int v)
+    {
+        value=v;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+class BST
+{
+    TreeNode*root=NULL;
+    BST()
+    {
+        root=NULL;
+    }
+
+    void insertion(TreeNode* nnode)
+    {
+        if(root==NULL)
+        {
+        root=nnode;
+        cout<<"Value inserted as root node."<<endl;
+        }
+        else
+        {
+            TreeNode*temp=root;
+            while(temp!=NULL)
+            {
+                if((nnode->value<temp->value) && (temp->left==NULL))
+                {
+                    temp->left=nnode;
+                    break;
+                }
+                else if(nnode->value<temp->value)
+                temp=temp->left;
+                else if((nnode->value>temp->value)&&(temp->right==NULL))
+                {
+                    temp->right=nnode;
+                    break;
+                }
+                else
+                temp=temp->right;
+
+            }
+        }
+    }
+
+    void print2D(TreeNode*r, int space)
+    {
+        if(r==NULL)
+        return;
+        space+=10;
+        print2D(r->right,space);
+        cout<<endl;
+        for(int i=10;i<space;i++)
+        cout<<" ";
+        cout<<r->value<<"\n";
+        print2D(r->left,space);
+    }
+};
+
+int main()
+{
+    BST obj;
+    int val;
+    TreeNode* nnode=new TreeNode();
+    cout<<"Enter the value u want to enter:";
+    cin>>val;
+    nnode->value=val;
+}
